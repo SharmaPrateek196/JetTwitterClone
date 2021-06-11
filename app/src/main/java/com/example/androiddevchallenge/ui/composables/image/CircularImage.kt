e32b4d32fe39cf15245a25f4248d31e9ccfbebcc
SharmaPrateek196 <sharmaprateek196@gmail.com>
2021-06-11 17:56:53 +0530

    update: Coil dependency and usage

--- app/src/main/java/com/example/androiddevchallenge/ui/composables/image/CircularImage.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/composables/image/CircularImage.kt
@@ -1,19 +1,14 @@
 package com.example.androiddevchallenge.ui.composables.image
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.ui.theme.circularShape
-import dev.chrisbanes.accompanist.coil.CoilImage
+import com.google.accompanist.coil.rememberCoilPainter
 
 @Composable
 fun CircularImage(
@@ -21,8 +16,8 @@ fun CircularImage(
     modifier: Modifier,
     imageViewSize: Int
 ) {
-    CoilImage(
-        data = imageResource,
+    Image(
+        painter = rememberCoilPainter(imageResource) ,
         null,
         contentScale = ContentScale.FillBounds,
         modifier = modifier
--- app/src/main/java/com/example/androiddevchallenge/ui/composables/image/TweetImage.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/composables/image/TweetImage.kt
@@ -1,26 +1,20 @@
 package com.example.androiddevchallenge.ui.composables.image
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.ui.theme.rectangulerShapeRounderCorner
-import dev.chrisbanes.accompanist.coil.CoilImage
+import com.google.accompanist.coil.rememberCoilPainter
 
 @Composable
 fun TweetImage(
     imageResource: String,
     modifier: Modifier
 ) {
-        CoilImage(
-            data = imageResource,
+        Image(
+            painter = rememberCoilPainter(imageResource),
             contentDescription = null,
             modifier = modifier.clip(
                 rectangulerShapeRounderCorner
