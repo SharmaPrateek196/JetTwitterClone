7ce2310b0e4a95b3fb1941a28d9f1f1827359131
SharmaPrateek196 <sharmaprateek196@gmail.com>
2021-06-13 23:26:56 +0530

    update: Basic setup in SearchScreen

--- app/src/main/java/com/example/androiddevchallenge/ui/screens/searchscreen/SearchScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screens/searchscreen/SearchScreen.kt
@@ -1,51 +1,38 @@
 package com.example.androiddevchallenge.ui.screens.searchscreen
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.androiddevchallenge.TwitterApplication
+import com.example.androiddevchallenge.ui.theme.TwitterTheme
 import com.example.androiddevchallenge.utils.BottomNavVM
-import kotlinx.coroutines.launch
+import com.example.androiddevchallenge.utils.TwitterScaffold
 
 @Composable
 fun SearchScreen(
     searchScreenViewModel: SearchScreenVM = hiltViewModel(),
-    bottomNavViewModel: BottomNavVM = hiltViewModel()
+    bottomNavViewModel: BottomNavVM = hiltViewModel(),
+    baseApplication: TwitterApplication
 ) {
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
 
-//    TwitterTheme(
-//        darkTheme = baseApplication.isGlobalDarkTheme.value,
-//    ) {
-//        Scaffold(
-//            topBar = {
-//                MainTopAppBar(
-//                    onMenuClicked = {
-//                        scope.launch {
-//                            scaffoldState.drawerState.open()
-//                        }
-//                    }
-//                )
-//            },
-//            drawerBackgroundColor = MaterialTheme.colors.primary,
-//            scaffoldState = scaffoldState,
-//            drawerContent = {
-//                NavigationDrawer(
-//                    currentUser = me,
-//                    onThemeChanged = {
-//                        baseApplication.onThemeChanged()
-//                    },
-//                    baseApplication
-//                )
-//            }
-//        ) {
-//
-//
-//
-//        }
-//    }
+    TwitterTheme(
+        darkTheme = baseApplication.isGlobalDarkTheme.value
+    ) {
+        TwitterScaffold(
+            baseApplication = baseApplication,
+            scaffoldState = scaffoldState,
+            topBar = { /*TODO*/ }
+        ) {
+
+
+
+
+
+        }
+    }
+
 
 }
